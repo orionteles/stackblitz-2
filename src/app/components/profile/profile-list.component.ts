8acfcdb0f8db05e106a6fe6f91e7454f25fb8bed
Orion Teles de Mesquita <orionteles@gmail.com>
2025-05-17 23:46:54 -0300

    Adding DataTables to Profile List

--- src/app/components/profile/profile-list.component.ts
+++ src/app/components/profile/profile-list.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProfileService } from '../../services/profile.service';
 import { Profile } from '../../models/profile.model';
 import Swal from 'sweetalert2';
+import 'datatables.net';
+import 'datatables.net-bs4';
+
+declare var $: any;
 
 @Component({
   selector: 'app-profile-list',
@@ -12,13 +16,28 @@ import Swal from 'sweetalert2';
   templateUrl: './profile-list.component.html',
   styleUrls: ['./profile-list.component.css']
 })
-export class ProfileListComponent implements OnInit {
+export class ProfileListComponent implements OnInit, AfterViewInit {
   profiles: Profile[] = [];
+  dataTable: any;
 
   constructor(private profileService: ProfileService) {}
 
   ngOnInit() {
     this.profiles = this.profileService.getProfiles();
+  }
+
+  ngAfterViewInit() {
+    this.initializeDataTable();
+  }
+
+  private initializeDataTable() {
+    this.dataTable = $('#profilesTable').DataTable({
+      pageLength: 10,
+      lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, 'All']],
+      responsive: true,
+      dom: 'Bfrtip',
+      buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+    });
   }
 
   deleteProfile(id: number) {
@@ -35,6 +54,8 @@ export class ProfileListComponent implements OnInit {
         if (this.profileService.deleteProfile(id)) {
           Swal.fire('Deleted!', 'Profile has been deleted.', 'success');
           this.profiles = this.profileService.getProfiles();
+          this.dataTable.destroy();
+          this.initializeDataTable();
         }
       }
     });
